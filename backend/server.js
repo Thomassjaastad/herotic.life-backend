bdd71f32b3e940c72dbcc94e31dc3fd8b2806a5b
thomassjaastad <thomas.sjaastad@gmail.com>
2021-01-27 11:21:47 +0100

    add item

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,21 @@
-console.log('test')
+const express = require('express'); ;
+
+const items = [
+  {
+    id: '001',
+    image:
+      'https://www.herotic.life/uploads/1/2/1/8/121808125/s498120939118584775_p37_i8_w4267.jpeg?width=160',
+    title: 'Herotic Mølla Beanie Black',
+    price: '399,-',
+    currency: 'kr'
+  }
+];
+
+const app = express();
+app.get('/items', (req, res) => {
+  res.json(items);
+});
+
+const port = 3001;
+
+app.listen(port, () => console.log(`Server listening on port ${port}`));
