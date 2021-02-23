a0c6ec37b6b2080b7d8997e433c54e6ccf5b3c84
thomassjaastad <thomas.sjaastad@gmail.com>
2021-02-23 10:23:18 +0100

    include querystring for cart

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const queryString = require('query-string');
 const express = require('express');
 const app = express();
 
@@ -7,7 +8,7 @@ const items = [
     image:
       'https://www.herotic.life/uploads/1/2/1/8/121808125/s498120939118584775_p37_i8_w4267.jpeg?width=160',
     title: 'Herotic Mølla Beanie Black',
-    price: '399,-',
+    price: 399,
     currency: 'kr'
   },
   {
@@ -15,7 +16,23 @@ const items = [
     image:
       'https://www.herotic.life/uploads/1/2/1/8/121808125/s498120939118584775_p36_i13_w4608.jpeg?width=160',
     title: 'Herotic Mølla Beanie Navy',
-    price: '399,-',
+    price: 399,
+    currency: 'kr'
+  },
+  {
+    id: '003',
+    image:
+      'https://www.herotic.life/uploads/1/2/1/8/121808125/s498120939118584775_p34_i1_w2354.jpeg?width=160',
+    title: 'Herotic Daddy Cap Ochre',
+    price: 499,
+    currency: 'kr'
+  },
+  {
+    id: '004',
+    image:
+      'https://www.herotic.life/uploads/1/2/1/8/121808125/s498120939118584775_p19_i18_w160.jpeg',
+    title: 'Herotic Classic White Hood *Glow in the dark*',
+    price: 799,
     currency: 'kr'
   }
 ];
@@ -28,6 +45,19 @@ app.get('/items/:id', (req, res) => {
   const productId = req.params.id;
   const result = items.filter((item) => item.id === productId);
   res.json(result[0]);
+});
+
+app.get('/cart', (req, res) => {
+  const prodId = [].concat(
+    queryString.parse(`id=${req.query.id}`, {
+      arrayFormat: 'comma'
+    }).id
+  );
+
+  const result = prodId
+    .map((id) => items.find((item) => item.id === id))
+    .filter(Boolean);
+  res.json(result);
 });
 
 const port = 3001;
